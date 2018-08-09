27bcf658f6a9b90833a013bbfbe5b81e9aacce87
Ajay Thankiah Saraswathy Perumal <A1809750@mail.niu.edu>
2018-08-08 19:29:54 -0500

    Supporting Generic services to CGS Web applications

---
+++ GenericWebService/ControllerMethods.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web;
+
+namespace GenericWebService.Helpers
+{
+    public class ControllerMethods
+    {
+        private static readonly ConnectionStringSettings connectionStringSettingLocalhost;
+        private static readonly ConnectionStringSettings connectionStringSettingDev;
+        private static readonly ConnectionStringSettings connectionStringSettingProd;
+        private static readonly String connectionString;
+        private static readonly SqlConnection sqlConnection;
+        private static readonly string[] validIdArray = Convert.ToString(ConfigurationManager.AppSettings["validIds"]).Split(',');
+        private static readonly ArrayList validIdArrayList = new ArrayList(validIdArray);
+        private static readonly string host;
+
+        static ControllerMethods()
+        {
+            if(sqlConnection == null)
+            {
+                host = HttpContext.Current.Request.Url.Host;
+
+                foreach (ConnectionStringSettings connectionStringSetting in ConfigurationManager.ConnectionStrings)
+                {
+                    if (connectionStringSetting.Name.Contains("Localhost")) connectionStringSettingLocalhost = connectionStringSetting;
+                    if (connectionStringSetting.Name.Contains("Dev")) connectionStringSettingDev = connectionStringSetting;
+                    if (connectionStringSetting.Name.Contains("Prod")) connectionStringSettingProd = connectionStringSetting;
+                }
+                
+                if (host.Equals("localhost"))
+                {
+                    connectionString = (connectionStringSettingLocalhost != null) ? connectionStringSettingLocalhost.ConnectionString : "";
+                }
+                else if (host.Contains("dev"))
+                {
+                    connectionString = (connectionStringSettingDev != null) ? connectionStringSettingDev.ConnectionString : "";
+                    
+                }else
+                {
+                    connectionString = (connectionStringSettingProd != null) ? connectionStringSettingProd.ConnectionString : "";
+                }
+                if(connectionString == "" && ConfigurationManager.ConnectionStrings[0] != null) sqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings[0].ConnectionString);
+            } 
+        }
+
+        public static string OpenSqlConnection()
+        {
+            try
+            {
+                if(sqlConnection == null) return "Database connection failed! Notification email has been sent to the development team. Please try after some time.";
+                sqlConnection.Open();
+                return "";
+            }
+            catch (Exception)
+            {
+                CloseSql();
+                return "Database connection failed! Notification email has been sent to the development team. Please try after some time.";
+            }
+        }
+
+        public static void CloseSql()
+        {
+            if (sqlConnection != null)
+            {
+                sqlConnection.Close();
+            }
+        }
+
+
+        public static SqlDataReader RunProcedure(String procedureName, string[] parameterName, int?[] paramValue)
+        {
+            try
+            {
+                SqlCommand sqlCommand = new SqlCommand();
+                sqlCommand.Connection = sqlConnection;
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+                sqlCommand.CommandText = procedureName;
+                for (int i = 0; i < parameterName.Length; i++)
+                {
+                    sqlCommand.Parameters.Add(new SqlParameter(parameterName[i], paramValue[i]));
+                }
+                return sqlCommand.ExecuteReader();
+            }
+            catch (Exception)
+            {
+                CloseSql();
+                return null;
+            }
+        }
+
+        public static SqlDataReader RunProcedure(String procedureName, string parameterName, int? paramValue)
+        {
+            try
+            {
+                SqlCommand sqlCommand = new SqlCommand();
+                sqlCommand.Connection = sqlConnection;
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+                sqlCommand.CommandText = procedureName;
+                sqlCommand.Parameters.Add(new SqlParameter(parameterName, paramValue));
+                return sqlCommand.ExecuteReader();
+            }
+            catch (Exception)
+            {
+                CloseSql();
+                return null;
+            }
+        }
+
+        public static SqlDataReader RunProcedure(String procedureName)
+        {
+            try
+            {
+                SqlCommand sqlCommand = new SqlCommand();
+                sqlCommand.Connection = sqlConnection;
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+                sqlCommand.CommandText = procedureName;
+                return sqlCommand.ExecuteReader();
+            }
+            catch (Exception)
+            {
+                CloseSql();
+                return null;
+            }
+        }
+
+        public static bool IsValidId(ref int? id)
+        {
+            if (!id.HasValue)
+                id = Convert.ToInt32(ConfigurationManager.AppSettings["DefaultId"]);
+            if (!validIdArrayList.Contains(id.ToString()))
+            {
+                id = Convert.ToInt32(ConfigurationManager.AppSettings["DefaultId"]);
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ GenericWebService/EmailHelper.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Configuration;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Web;
+
+namespace GenericWebService.Helpers
+{
+    public class EmailHelper
+    {
+        public static bool EmailErrorMessage(string message, String messageShownToUser)
+        {
+            string url = HttpContext.Current.Request.Url.AbsoluteUri;
+            string path = HttpContext.Current.Request.Url.AbsolutePath;
+            string host = HttpContext.Current.Request.Url.Host;
+            if (host.Equals("localhost")) return false;
+            string subject = "error in the path " + path;
+            SmtpClient client = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(ConfigurationManager.AppSettings["adminEmail"], ConfigurationManager.AppSettings["adminPassword"])
+            };
+            string[] developerEmails = ConfigurationManager.AppSettings["DeveloperEmailsToReceiveErrorMessages"].ToString().Split(',');
+
+            try
+            {
+                MailMessage mail = new MailMessage();
+                mail.From = new MailAddress(ConfigurationManager.AppSettings["adminEmail"]);
+
+                for (int i = 0; i < developerEmails.Length; i++)
+                {
+                    if (i == 0) mail.To.Add(developerEmails[i]);
+                    else mail.CC.Add(developerEmails[i]);
+                }
+
+                mail.Subject = subject;
+                StringBuilder emailBody = new StringBuilder();
+                emailBody.Append("Team,\n\t");
+                emailBody.Append("An error triggered in the url - ");
+                emailBody.Append(url);
+                emailBody.Append(", in the host - ");
+                emailBody.Append(host);
+                emailBody.Append("\n\n");
+                emailBody.Append("The error message shown to the user is : '");
+                emailBody.Append(messageShownToUser);
+                emailBody.Append("'\n\n");
+                emailBody.Append("The detailed error message is as follows:\n");
+                emailBody.Append(message);
+                emailBody.Append("\n\n\n\nCenter for Governmental Studies\n");
+                mail.Body = emailBody.ToString();
+                client.Send(mail);
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
